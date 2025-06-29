c40d94644e7c8df470fd9867a499ef061801ac73
Asoh-Acfs <asohfrancisca06@gmail.com>
2025-06-29 06:20:13 +0100

    Update sass.py

--- sass.py
+++ sass.py
@@ -10,6 +10,7 @@ st.markdown(
          
         url("https://i.postimg.cc/sXFhvNFd/28b4b66b94acdfb7f139bb1ed9ff6bf6.gif"),
         url("https://i.postimg.cc/0jSxrVq3/Screenshot-20250629-061325.jpg");
+        url("https://i.postimg.cc/Z9S6BMdY/Screenshot-20250629-030117.jpg");
         
         background-size: cover, cover, cover;
         background-repeat: no-repeat, no-repeat, no-repeat;
